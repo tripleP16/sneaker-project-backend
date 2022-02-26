c62b7241fe54b01ff4cd70c0b3eb142e57b535f5
Pablo Perez <perez51160900@hotmail.com>
2022-02-26 12:41:27 -0400

    añadiendo la entidad de usuarios

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,4 +1,15 @@
-import { Module } from '@nestjs/common';
-
-@Module({})
+import { Inject, Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+@Module({
+  imports: [
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (config: ConfigService) => ({
+        uri: config.get('URI'),
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+})
 export class DatabaseModule {}
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { UsersController } from './controllers/users.controller';
+import User, { UserSchema } from './models/user.entity';
 import { UsersService } from './services/users.service';
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService]
+  providers: [UsersService],
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: User.name,
+        schema: UserSchema,
+      },
+    ]),
+  ],
 })
 export class UsersModule {}
