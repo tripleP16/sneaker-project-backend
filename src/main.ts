d2c4c135d0d1ae4348b28d210d7099522185c1e7
Pablo Perez <perez51160900@hotmail.com>
2022-03-07 12:15:33 -0400

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -16,6 +16,6 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, document);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
-  await app.listen(process.env.PORT);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
